eb5e7a2274fdf3fda018ceeb3b0625364a1ff228
Jin An <jinsungan@gmail.com>
2020-10-28 08:23:28 -0400

    Create DictionaryTestDriver.java

---
+++ dictionary/DictionaryTestDriver.java
@@ -0,0 +1,6 @@
+public class DictionaryTestDriver {
+
+    public static void main(String[] args) {
+        Dictionary<Hangar, Airplane> dictionary = new HashDictionary<Hangar, Airplane>();
+    }
+}
