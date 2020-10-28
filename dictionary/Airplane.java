5904e83454e5e02d59f32dd70ed293492a8e7edb
Jin An <jinsungan@gmail.com>
2020-10-28 08:24:22 -0400

    Create Airplane.java

---
+++ dictionary/Airplane.java
@@ -0,0 +1,9 @@
+public class Airplane {
+    private String airline;
+    private int flightNum;
+
+    public Airplane(String airline, int flightNum) {
+        this.airline = airline;
+        this.flightNum = flightNum;
+    }
+}
