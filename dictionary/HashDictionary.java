91e5c987dfb8e929777b8eec34cb259abf2182f3
Jin An <jinsungan@gmail.com>
2020-10-28 08:23:47 -0400

    Create HashDictionary.java

---
+++ dictionary/HashDictionary.java
@@ -0,0 +1,22 @@
+public class HashDictionary <K extends Comparable<K>, V> implements Dictionary <K, V> {
+
+    @Override
+    public V search(K key) {
+
+        for (loop) {
+            if (this.key.compareTo(key))
+        }
+
+        return null;
+    }
+
+    @Override
+    public void remove(K key) {
+
+    }
+
+    @Override
+    public void insert(K key, V value) {
+
+    }
+}
